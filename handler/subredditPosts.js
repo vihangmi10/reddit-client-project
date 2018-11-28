521131879363b8a5d3e0a1cc2e812b46e55cc83d
vihangmirkhelkar <vihangmirkhelkar@gmail.com>
2018-11-27 16:35:07 -0800

    New implementation for pagination method. Backend ready.

--- handler/subredditPosts.js
+++ handler/subredditPosts.js
@@ -1,27 +1,40 @@
 import request from 'request-promise';
 
-const number_of_reviews_per_page = 10;
-
 const getNewPosts = async (req, typeOfPost) => {
-    let arrayOfPosts = [];
+    let before = null;
+    let queryParameter = {};
     let redditnewPostURL = `http://www.reddit.com/r/all/${typeOfPost}.json`;
     let page_number = req.query.page ? req.query.page : 1;
-    let postsFromReddit = await request.get(redditnewPostURL);
+
+    if (page_number > 1 && req.query.before || req.query.after){
+       if (req.query.before){
+           queryParameter['before'] = req.query.before;
+       } else {
+           queryParameter['after'] = req.query.after;
+       }
+    }
+    let requestOptions = {
+        uri: redditnewPostURL,
+        qs: queryParameter
+    };
+    let postsFromReddit = await request.get(requestOptions);
     let postsInJson = JSON.parse(postsFromReddit);
     const total_posts = postsInJson.data.dist;
     let posts = postsInJson.data.children;
-    if (page_number * number_of_reviews_per_page > total_posts) {
-        throw new Error('Reviews exceeded');
-    }
-    else {
-        if (page_number === 1) {
-            arrayOfPosts = posts.slice(0,(number_of_reviews_per_page * page_number));
-        }else {
-            arrayOfPosts = posts.slice(((number_of_reviews_per_page * page_number) - (number_of_reviews_per_page)), (number_of_reviews_per_page * page_number));
-        }
 
-        console.log('SLICED ARRAY IS ------ ', arrayOfPosts);
+    let after = postsInJson.data.after ? postsInJson.data.after : posts[total_posts-1].data.name;
+
+    if(page_number > 1) {
+        before = postsInJson.data.before ? postsInJson.data.before : posts[0].data.name;
     }
-    return postsFromReddit;
+
+    let subRedditObject = {
+        total_subreddits: total_posts,
+        previousPosts: before,
+        nextPosts: after,
+        redditPosts: posts
+    };
+
+    return subRedditObject;
 };
 export default getNewPosts;
