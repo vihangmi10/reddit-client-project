90f3b0c38e08b44d79b73aee93b887c4a65ca0d8
vihangmirkhelkar <vihangmirkhelkar@gmail.com>
2018-11-28 11:54:02 -0800

    Add URLs for after and before for pagination on the front end.

--- handler/subredditPosts.js
+++ handler/subredditPosts.js
@@ -2,6 +2,7 @@ import request from 'request-promise';
 
 const getNewPosts = async (req, typeOfPost) => {
     let before = null;
+    let beforeURL = null;
     let queryParameter = {};
     let redditnewPostURL = `http://www.reddit.com/r/all/${typeOfPost}.json`;
     let page_number = req.query.page ? req.query.page : 1;
@@ -21,19 +22,25 @@ const getNewPosts = async (req, typeOfPost) => {
     let postsInJson = JSON.parse(postsFromReddit);
     const total_posts = postsInJson.data.dist;
     let posts = postsInJson.data.children;
-    console.log('Posts ---- ', posts);
+    //console.log('Posts ---- ', posts);
     let after = postsInJson.data.after ? postsInJson.data.after : posts[total_posts-1].data.name;
+    let afterURL = `http://localhost:3000/${typeOfPost}?after=${after}`;
 
     if(page_number > 1) {
         before = postsInJson.data.before ? postsInJson.data.before : posts[0].data.name;
+        beforeURL = `http://localhost:3000/${typeOfPost}?before=${before}`;
     }
 
     let subRedditObject = {
         total_subreddits: total_posts,
         previousPosts: before,
+        previousPostsURL: beforeURL,
         nextPosts: after,
+        nextPostsURL: afterURL,
         redditPosts: posts
     };
+    console.log('AFTER URL ----- ', afterURL);
+    console.log('BEFORE URL ----- ', beforeURL);
     return subRedditObject;
 };
 export default getNewPosts;
